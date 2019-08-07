ead656d3d88ff3e6b7b847b5851b29953559537b
RVD246 <v.raveendran@student.saedubai.com>
2019-08-07 19:01:56 +0400

    TBRPG

---
+++ TBRPG/Assets/Scenes/Scripts/AI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] waypoints;
+
+    [SerializeField]
+    float speed;
+
+    PLAYENEMY enemy;
+
+    float[] distances;
+    float shortest;
+
+    int index = 0;
+    bool inverse = false;
+
+    Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = (waypoints[index].transform.position - transform.position).normalized;
+        enemy = FindObjectOfType<PLAYENEMY>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, waypoints[index].transform.position);
+        if (enemy.wayactivate)
+        {
+            if (distance < 0.25f)
+            {
+                if (!inverse)
+                {
+                    if (index >= waypoints.Length - 2)
+                        inverse = true;
+                    index++;
+                }
+                else
+                {
+                    if (index <= 1)
+                        inverse = false;
+                    index--;
+                }
+                direction = (waypoints[index].transform.position - transform.position).normalized;
+            }            
+            transform.position += direction * speed * Time.deltaTime;
+            transform.LookAt(waypoints[index].transform);
+        }
+        else
+        {
+            direction = (waypoints[index].transform.position - transform.position).normalized;
+        }
+    }
+}
---
+++ TBRPG/Assets/Scenes/Scripts/PLAYENEMY.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PLAYENEMY : MonoBehaviour
+{
+    GameObject player;
+
+    GameObject enemy;
+
+    GameObject mainCamera;
+
+    [HideInInspector] public bool wayactivate;
+
+    Animator anim;
+
+    RaycastHit hit;
+    [SerializeField] NavMeshAgent agent;
+
+    public float enemyspeed;
+
+    [SerializeField] Vector3 vectoroffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemy = GameObject.FindGameObjectWithTag("enemy");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        mainCamera.transform.position = player.transform.position + vectoroffset;
+        anim = player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {     		
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        mainCamera.transform.position = player.transform.position + vectoroffset;
+
+        mainCamera.transform.LookAt(player.transform);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("clicked", true);
+            if (Physics.Raycast(Camera.main.transform.position, cameraRay.direction, out hit, Mathf.Infinity, LayerMask.GetMask("Floor")))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (agent.remainingDistance == 0)
+            anim.SetBool("clicked", false);
+
+        if (Vector3.Distance(player.transform.position, enemy.transform.position) < 10)
+        {
+            wayactivate = false;
+
+            Vector3 direction = (player.transform.position - enemy.transform.position).normalized;
+
+            enemy.transform.LookAt(player.transform);
+            enemy.transform.position += direction * enemyspeed * Time.deltaTime;                              
+        }
+        else
+            wayactivate = true;
+    }
+}
